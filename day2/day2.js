cff871f2b552eed89d2c89d9fa466bffad1ea615
Kristen Monnik <nolaviolinist@gmail.com>
2021-12-08 14:43:28 -0500

    add day 2 solution

--- day2/day2.js
+++ day2/day2.js
@@ -3,13 +3,62 @@ const fs = require('fs')
 const lines = fs
     .readFileSync("input.txt", { encoding: "utf-8" })
     .split("\n")
-    .filter((x) => Boolean(x));
+    .filter((x) => Boolean(x))
+    .map((x) => {
+        const [direction, n] = x.split(" "); //splits each line on the space
+        return {
+            direction,
+            n: parseInt(n)
+        };
+    });
 
-console.log(lines);
+const submarine = {
+    position: 0,
+    depth: 0
+};
+
+for (const line of lines) {
+    switch (line.direction) {
+        case "forward":
+            submarine.position += line.n; //to add n to "forward" total
+            break;
+        case "up":
+            submarine.depth -= line.n;
+            break;
+        case "down":
+            submarine.depth += line.n;
+            break;
+    }
+}
+
+console.log(submarine.position * submarine.depth);
+
+//part 2
+let sub = {
+    position: 0,
+    depth: 0,
+    aim: 0
+};
+
+for (const line of lines) {
+    switch (line.direction) {
+        case "forward":
+            sub.position += line.n; //to add n to "forward" total
+            sub.depth += sub.aim * line.n;
+            break;
+        case "up":
+            sub.aim -= line.n;
+            break;
+        case "down":
+            sub.aim += line.n;
+            break;
+    }
+}
+
+console.log(sub.position * sub.depth);
 
 
-//  for (i = 0; i > lines.length; i++) {
-//     let direction = [i];
 
-//  }
+
+
 
